545b92ed75b9e5db6919a3efcb45b3794ead16f6
ico <enricodparolin@gmail.com>
2025-03-20 21:08:17 -0300

    atualizacao do banco de dados

--- db/3info.sql
+++ db/3info.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 18-Mar-2025 às 12:31
+-- Tempo de geração: 21/03/2025 às 01:07
 -- Versão do servidor: 10.4.32-MariaDB
--- versão do PHP: 8.2.12
+-- Versão do PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `compras`
+-- Estrutura para tabela `compras`
 --
 
 CREATE TABLE `compras` (
@@ -33,24 +33,38 @@ CREATE TABLE `compras` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Despejando dados para a tabela `compras`
+--
+
+INSERT INTO `compras` (`id`, `item`) VALUES
+(4, 'abacati'),
+(5, 'masã'),
+(6, 'pineapple apple pen'),
+(7, 'laranza'),
+(8, 'jamal'),
+(9, 'rabanette'),
+(10, 'senora'),
+(11, 'kiui');
+
+--
 -- Índices para tabelas despejadas
 --
 
 --
--- Índices para tabela `compras`
+-- Índices de tabela `compras`
 --
 ALTER TABLE `compras`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT de tabelas despejadas
+-- AUTO_INCREMENT para tabelas despejadas
 --
 
 --
 -- AUTO_INCREMENT de tabela `compras`
 --
 ALTER TABLE `compras`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
