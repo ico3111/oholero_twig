05e85aa482221482375d9cbcb836b3e2f10ac14e
Enrico Parolin <125042732+ico3111@users.noreply.github.com>
2025-04-20 01:55:03 -0300

    Update user_edit.php

--- user_edit.php
+++ user_edit.php
@@ -10,18 +10,19 @@ $senha_atual = $_POST['senha_atual'] ?? null;
 $senha_nova = $_POST['senha_nova'] ?? null;
 $confirma_senha_nova = $_POST['confirma_senha_nova'] ?? null;
 
-if (empty($id) || empty($login) || empty($senha_atual) || empty($senha_nova) || empty($confirma_senha_nova)) { header('Location: user.php'); exit; }
+if (empty($id) || empty($login) || empty($senha_atual) || empty($senha_nova) || empty($confirma_senha_nova)) { header('Location: user.php?erro=campos_vazios'); exit; }
 if (!password_verify($senha_atual, $_SESSION['user']['senha'])) { header('Location: user.php?erro=senha_incorreta'); exit; }
 if ($senha_nova !== $confirma_senha_nova) { header('Location: user.php?erro=senha_diferente'); exit; }
 
 $query = $pdo->prepare('UPDATE users SET login = :login, senha = :senha WHERE id = :id');
 $query->bindValue(':id', $id);
 $query->bindValue(':login', $login);
-$query->bindValue(':senha', password_hash($senha_nova, PASSWORD_DEFAULT));
+$senha_trocada = password_hash($senha_nova, PASSWORD_DEFAULT);
+$query->bindValue(':senha', $senha_trocada);
 $query->execute();
 
 $_SESSION['user']['login'] = $login;
-$_SESSION['user']['senha'] = password_hash($senha_nova, PASSWORD_DEFAULT);
+$_SESSION['user']['senha'] = $senha_trocada;
 
 header('Location: user.php?sucesso=1');
 exit;
